991178a4b00101af8c44016ae80316522d223654
CharlesGhanime <ghanime.charles@gmail.com>
2019-04-14 12:47:18 +0200

    on commence les floats :'(

--- tests/main_test_comportement.c
+++ tests/main_test_comportement.c
@@ -6,7 +6,7 @@
 /*   By: cghanime <cghanime@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/15 15:42:17 by cghanime          #+#    #+#             */
-/*   Updated: 2019/03/28 13:34:29 by cghanime         ###   ########.fr       */
+/*   Updated: 2019/04/14 12:40:35 by cghanime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,27 @@
 
 int main ()
 {
-	char *ptr;
-	printf("%p", ptr);
+	int f = 42;
+	printf("***********************Gestion des Floats**********************\n");
+	printf("%-6.4d\n", f);
+	printf("***********************Tests decalage binaire******************\n");
+	int b = 0;
+	printf("%d\n", b << 7);
+	b = 1 << 7;
+	printf("%d\n", b);
+	if ((b == 1 << 5) & 1)
+		printf("CONDITION IS TRUE\n");
+	printf("%d\n", b << 0);
+	printf("%d\n", b << 1);
+	printf("%d\n", b << 2);
+	printf("%d\n", b << 3);
+	printf("%d\n", b << 4);
+	printf("%d\n", b << 5);
+	printf("%d\n", b << 6);
+	printf("%d\n", b << 7);
+//	printf ("%d\n", b << 5);
+	if (b << 1)
+		printf("Working");
+
 	return (0);
 }
