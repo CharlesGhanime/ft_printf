a16e21b8d9c02cd3b85f542384fcd0d238c4eb6c
Alban BOITIER <aboitier@e3r11p4.42.fr>
2019-06-21 01:17:11 +0200

    new file created for better readibility

--- srcs/get_var.c
+++ srcs/get_var.c
@@ -5,29 +5,130 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: aboitier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/07 16:43:13 by aboitier          #+#    #+#             */
-/*   Updated: 2019/06/07 16:55:06 by aboitier         ###   ########.fr       */
+/*   Created: 2019/06/20 21:37:51 by aboitier          #+#    #+#             */
+/*   Updated: 2019/06/21 01:06:01 by aboitier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		lobby(t_ptf *head, va_list ap)
+#include "../includes/ft_printf.h"
+
+int     get_type(t_ptf **word, va_list arg)
 {
-	t_ptf	*percents;
-
-	percents = head->next;
-	while (percents->next)
+	if ((*word)->conv == 'c')
 	{
-						
-		
+		if (!((*word)->a_t.a_int = va_arg(arg, int)))
+			return (-1);
+	}
+	else if ((*word)->conv == 'p')
+	{
+		if (!((*word)->a_t.a_ptr = va_arg(arg, void *)))
+			return (-1);
+	}
+	else if ((*word)->conv == 's')
+	{
+		if (!((*word)->a_t.a_string = va_arg(arg, char *)))
+			return (-1);
+	}
+	else if ((*word)->conv == 'f')
+		return (get_f_type(&(*word), arg));
+	else if (((*word)->conv == 'd' || (*word)->conv == 'i' || (*word)->conv == 'o'
+				|| (*word)->conv == 'u' || (*word)->conv == 'x' || (*word)->conv == 'X'))
+		return (get_dioux_type(&(*word), arg));
+	return (0);
+}
 
-		percents = percents->next;
+int     get_f_type(t_ptf **word, va_list arg)
+{
+	if (!((*word)->flags) || ((*word)->flags[0] != 'l'
+				&& (*word)->flags[0] != 'L'))
+	{
+		if (!((*word)->a_t.a_double = va_arg(arg, double)))
+			return (-1);
+	}
+	else if ((*word)->flags[0] == 'l')
+	{
+		if (!((*word)->a_t.a_double = va_arg(arg, double)))
+			return (-1);
+	}
+	else if ((*word)->flags[0] == 'L')
+	{
+		if (!((*word)->a_t.a_ldouble = va_arg(arg, long double)))
+			return (-1);
 	}
 
+	return (0);
 }
 
-int		get_unsigned(t_ptf *head)
+int     get_dioux_type(t_ptf **word, va_list arg)
 {
-	
-
-
+	if (((*word)->conv == 'd' || (*word)->conv == 'i'))
+	{
+		if (!(*word)->flags)
+		{
+			if (!((*word)->a_t.a_int = va_arg(arg, int)))
+				return (-1);
+		}
+		else
+			return ((get_di_type(&(*word), arg)));
+	}
+	else if ((*word)->conv == 'o' || (*word)->conv == 'u' ||  (*word)->conv == 'x'
+			|| (*word)->conv == 'X')
+	{
+		if (!(*word)->flags)
+		{
+			if (!((*word)->a_t.a_uint = va_arg(arg, unsigned int)))
+				return (-1);
+		}
+		else
+			return ((get_oux_type(&(*word), arg)));
+	}
+	return (0);
 }
+
+
+int     get_di_type(t_ptf **word, va_list arg)
+{
+	if ((*word)->flags[0] == 'h' && (*word)->flags[1] != 'h')
+	{
+		if (!((*word)->a_t.a_int = va_arg(arg, int)))
+			return (-1);
+	}
+	else if ((*word)->flags[0] == 'h' && (*word)->flags[1] == 'h')
+	{
+		if (!((*word)->a_t.a_int = va_arg(arg, int)))
+			return (-1);
+	}
+	else if ((*word)->flags[0] == 'l' && (*word)->flags[1] != 'l')
+	{
+		if (!((*word)->a_t.a_long = va_arg(arg, long)))
+			return (-1);
+	}
+	else if ((*word)->flags[0] == 'l' && (*word)->flags[1] == 'l')
+		if (!((*word)->a_t.a_llong = va_arg(arg, long long)))
+			return (-1);
+	return (0);
+}
+
+int     get_oux_type(t_ptf **word, va_list arg)
+{
+	if ((*word)->flags[0] == 'h' && (*word)->flags[1] != 'h')
+	{
+		if (!((*word)->a_t.a_int = va_arg(arg, int)))
+			return (-1);
+	}
+	else if ((*word)->flags[0] == 'h' && (*word)->flags[1] == 'h')
+	{
+		if (!((*word)->a_t.a_int = va_arg(arg, int)))
+			return (-1);
+	}
+	else if ((*word)->flags[0] == 'l' && (*word)->flags[1] != 'l')
+	{
+		if (!((*word)->a_t.a_ulong = va_arg(arg, unsigned long)))
+			return (-1);
+	}
+	else if ((*word)->flags[1] == 'l' && (*word)->flags[1] == 'l')
+		if (!((*word)->a_t.a_ullong = va_arg(arg, unsigned long long)))
+			return (-1);
+	return (0);
+}
+
