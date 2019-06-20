068f01684b53e4af643ee7d8f5b38aea66114f81
CharlesGhanime <ghanime.charles@gmail.com>
2019-06-20 19:00:49 +0200

    pourcent_p_debugg

--- .annex/main_ptr.c
+++ .annex/main_ptr.c
@@ -4,16 +4,19 @@
 
 int		main(void)
 {
-	char	*test;
+	int	test;
 	char	*prout;
 	char	*str;
-	void *ptr10 = test;
+	void *ptr10 = &test;
 	void *ptr03 = prout;
 	void *ptr02 = str;
-	void *ptr01 = NULL;
+	void *ptr01;
 	void *ptr00 = NULL;
 
-ft_printf("p NULL; = %p\np NULL ><; = %p\np NULL; = %p\np NULL ><; = %p\n", ptr00,ptr01,ptr02,ptr03);
-//ft_printf("p NULL; = %p\n", ptr10);
-	return (0);
+printf("p NULL; = %p\np NULL ><; = %p\np NULL; = %p\np NULL ><; = %p\n", ptr00,ptr01,&ptr02,&ptr03);
+printf("p NULL; = %p\n", (char *)ptr10);
+
+ft_printf("p NULL; = %p\np NULL ><; = %p\np NULL; = %p\np NULL ><; = %p\n", ptr00,ptr01,&ptr02,&ptr03);
+ft_printf("p NULL; = %p\n", (char *)ptr10);
+return (0);
 }
--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,14 +6,14 @@
 /*   By: cghanime <cghanime@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/20 18:58:27 by cghanime          #+#    #+#             */
-/*   Updated: 2019/06/19 16:47:54 by aboitier         ###   ########.fr       */
+/*   Updated: 2019/06/20 16:17:59 by cghanime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-#include "../libft/libft.h"
+#include "../Libft/libft.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
