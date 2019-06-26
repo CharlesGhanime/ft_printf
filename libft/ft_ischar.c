957b0e5a596ffcf2f4cc4a40f6d659e305de08bc
Alban BOITIER <aboitier@e3r6p18.42.fr>
2019-06-26 03:15:04 +0200

    slight modifs on ischar and addonechar

--- libft/ft_ischar.c
+++ libft/ft_ischar.c
@@ -6,7 +6,7 @@
 /*   By: aboitier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/19 18:44:45 by aboitier          #+#    #+#             */
-/*   Updated: 2019/06/25 07:03:44 by aboitier         ###   ########.fr       */
+/*   Updated: 2019/06/26 03:08:15 by aboitier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,13 @@
 int		ft_ischar(char *str, char c)
 {
 	int i;
-
-	printf("str === %s\n", str);
 	i = -1;
 	if (str)
 	{	
+
 		while (str[++i])
 			if (str[i] == c)
-			{
-				printf("\nstr[i] = %c\n", str[i]);
 				return (1);
-			}
 	}
 	return (0);
 }
