5bbf1c07bd43036e16d8259d369d60c145ce7eae
Alban BOITIER <aboitier@e1r12p1.42.fr>
2019-08-09 05:49:01 +0200

    int_long_print.c partially cleaned

--- srcs/int_long_print.c
+++ srcs/int_long_print.c
@@ -6,45 +6,12 @@
 /*   By: cghanime <cghanime@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/31 10:35:39 by cghanime          #+#    #+#             */
-/*   Updated: 2019/08/08 04:52:24 by aboitier         ###   ########.fr       */
+/*   Updated: 2019/08/09 00:57:15 by aboitier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 #include "../libft/libft.h"
-
-
-//int		get_token(t_ptf *percents) 
-//{
-//	int token;
-//
-//	token = 0;
-//	if (percents->width > (long)ft_strlen(percents->a_t.a_string) 
-//			&& percents->options & MINUS)
-//		token = 0;
-//	else if (percents->width <= 0 || (ft_ischar(percents->options, '-') == 1))
-//		token = 1;
-//	return (token);
-//}
-
-/* PRINT ADDRESS */
-
-int		ft_print_address(t_ptf *percents)
-{
-	char *str;
-
-	if (!(str = ft_strdup(ft_itoa_base((long)percents->a_t.a_ptr, "0123456789abcdef"))))
-		return (FALSE);
-//	printf("retour de ft_itoa_base = %s\n", str);
-//	printf("\nstr = %s\n", str);
-//	printf("printf prog = %p\n", percents->a_t.a_ptr);
-	percents->size = ft_strlen(str) + 2;
-	ft_putstr("0x");
-	ft_putstr(str);
-//	printf("\n\n");
-	free(str);
-	return (TRUE);
-}
 
 /* PRINT DECIMAL */
 
@@ -77,20 +44,6 @@ char	*get_int_var(t_ptf *percents)
 	return (str);
 }
 
-//int		count_pos(char *str, int token)
-//{
-//	int pos;
-//
-//	pos = 0;
-//	if (token == 0)
-//		while (str[pos + 1] == ' ' || str[pos + 1] == '0')
-//			pos++;
-//	else if (token == 1)
-//		while (str[pos + 1] == ' ')
-//			pos++;
-//	return (pos);
-//}
-
 //char	*str_width(char *str, long width, t_ptf *percents, char c)
 //{
 //	char 	*new;
@@ -122,7 +75,6 @@ char 	*int_precision(char *str, long precision, int len, t_ptf *percents)
 	
 	if ((!precision || (int)precision < len))
 		return (str);
-//	printf("PRECISION ======%d\n", precision);
 	if (!(new = (char *)malloc(sizeof(char) * (precision + len + 1))))
 		return (NULL);
 	i = -1;
@@ -136,8 +88,6 @@ char 	*int_precision(char *str, long precision, int len, t_ptf *percents)
 	while (++i < ((int)precision - len))
 		new[i] = '0';
 	new[i] = '\0';
-//	printf("new = %s\n", new);
-//	printf("WOOW2 == %s\n", new);
 	new = ft_strcat(new, str);
 	free(str);
 	return (new);
@@ -148,50 +98,41 @@ char	*int_width(char *str, long width, long len, t_ptf *percents)
 	char	*new;
 	char	*padd;
 	long	diff;
-	
+	int		min;
+
 	diff = width - len;
-	if (diff < 0 || !width)
+	if (diff < 0 || !width || (width - 1 == len && percents->options & PLUS))
 		return (str);
-//	printf("\nITIZ3 == %s\n", str); 
+	min = 0;
+	new = NULL;
 	if (percents->options & ZERO && !(percents->precision) && !(percents->options & MINUS))
-		padd = ft_padding(width - len, '0'); 
+		padd = ft_padding(diff, '0'); 
 	else
-		padd = ft_padding(width - len, ' ');
+		padd = ft_padding(diff, ' ');
+//	printf("ITIZs == %s\n", new);
+//	printf("LEN === %ld\tWIDTH === %ld\n", len, width);
 	if (!(percents->options & MINUS))
 	{
+		if (str[0] == '-' && padd && padd[0] == '0')
+			min = 1;
 		if (!(new = ft_strjoinfrchoz(padd, str, 1)))
 			return (NULL);
+		if (min == 1)
+		{
+			new[diff] = '0';
+			new[0] = '-';
+		}
 	}
 	else if (width > len)
+	{
 		if (!(new = ft_strjoinfrchoz(str, padd, 2)))
 			return (NULL);
+	}
+	else if (padd)
+		free(padd);
 	if (!new)
 		return (str);
 	free(str);
-//	printf("manage str = \n");
-
-	return (new);
-}
-
-char	*manage_opts(char *str, t_ptf *percents)
-{
-//	int		pos;
-	char	*new;
-
-	new = NULL;
-//	printf("manage str = %s\n", str);
-	if (percents->options & HASH && (percents->conv == 'x' || percents->conv == 'X' 
-				|| percents->conv == 'o' ))
-		;  												// input funct to manage HASH
-	if (percents->options & SPACE && !(percents->options & PLUS) && 
-			!(percents->unsignd) && (percents->signe == '+' || !str))
-		new = ft_addonecharpos(&str, ' ', 0);					// add one space
-	if (percents->options & PLUS && !(percents->unsignd) && str[0] != '-')
-		new = ft_strjoinfrchoz(&percents->signe, str, 2);		// print sign
-
-
-	if (!new)
-		return (str);
 	return (new);
 }
 
@@ -212,139 +153,8 @@ int		ft_print_decimal(t_ptf *percents)
 	if (!(str = manage_opts(str, percents)))
 		return (FALSE);
 //	printf("\nITIZ4 == %s\n", str); 
-
-
-  percents->size = ft_strlen(str);
-//	printf("\n"_RED"HEREEE"_END"\n");
+  	percents->size = ft_strlen(str);
 	ft_putstr(str);
 	free(str);
 	return (TRUE);
 }
-
-
-//char 	*int_precision(char *str, long precision)
-//{
-//	char *new;
-//	int i;
-//	int diff;
-//
-//	if (!precision || precision < ft_strlen(str))
-//		return (str);
-//	if (!(new = (char *)malloc(sizeof(char) * precision + 1)))
-//	i = -1;
-//	if (!(new = 
-//	diff = (size_t)precision - ft_strlen(str);
-//	while (str[++i] && i < (int)precision)
-//	{			
-//	}	
-//
-//	return (new);
-//}
-
-///* PRINT INTEGER */
-//
-//int		ft_print_integer(t_ptf *percents)
-//{
-//	char *str;
-//
-//	percents->size = ft_strlen(ft_itoa(va_arg(arg, int)));
-//	str = (char *)malloc(sizeof(char) * percents->size + 1);
-//	str = ft_itoa(va_arg(arg, int));
-//	ft_putstr(ft_width_precision(str, percents, percents->size));
-//	return (TRUE);
-//}
-//
-///* PRINT OCTAL */
-//
-//int		ft_print_octal(t_ptf *percents)
-//{
-//	char *str;
-//
-//	percents->size = ft_strlen(ft_itoa(va_arg(arg, int)));
-//	str = (char *)malloc(sizeof(char) * percents->size + 1);
-//	str = ft_itoa_base(va_arg(arg, int), 8);
-//	ft_hashtag_flag(percents);
-//	ft_zero_flag(percents, percents->size);
-//	ft_putstr(str);
-//	ft_minus_flag(percents, percents->size);
-//	return (TRUE);
-//}
-//
-///* PRINT UNSIGNED */
-//
-//int		ft_print_unsigned(t_ptf *percents)
-//{
-//	char *str;
-//
-//	percents->size = ft_strlen(ft_itoa(va_arg(arg, int)));
-//	str = (char *)malloc(sizeof(char) * percents->size + 1);
-//	str = ft_itoa_base(va_arg(arg, int), 10);
-//	ft_hashtag_flag(percents);
-//	ft_zero_flag(percents, percents->size);
-//	ft_putstr(str);
-//	ft_minus_flag(percents, percents->size);
-//	return (TRUE);
-//}
-//
-///* PRINT HEXA */
-//
-//int		ft_print_hexa(t_ptf *percents)
-//{
-//	char *str;
-//
-//	percents->size = ft_strlen(ft_itoa(va_arg(arg, int)));
-//	str = (char *)malloc(sizeof(char) * percents->size + 1);
-//	str = ft_itoa_base(va_arg(arg, int), 16);
-//	ft_hashtag_flag(percents);
-//	ft_zero_flag(percents, percents->size);
-//	ft_putstr(str);
-//	ft_minus_flag(percents, percents->size);
-//	return (TRUE);
-//}
-//
-///* PRINT HEXA MAJ */
-//
-//int		ft_print_hexa_maj(t_ptf *percents)
-//{
-//	char *str;
-//	int i;
-//
-//	percents->size = ft_strlen(ft_itoa_base(va_arg(arg, int), 16));
-//	str = (char *)malloc(sizeof(char) * percents->size + 1);
-//	str = ft_itoa_base(va_arg(arg, int), 16);
-//	i = 0;
-//	while (str[i])
-//	{
-//		if (str[i] >= 'a' && str[i] <= 'z')
-//			str[i] = str[i] - 32;
-//		i++;
-//	}
-//	ft_putstr(ft_width_precision(str, percents, percents->size));
-//	return (TRUE);
-//}
-//
-///* PRINT FLOAT */
-//
-//int		ft_print_float(t_ptf *percents)
-//{
-//	char *str;
-//	int i;
-//
-//	i = 0;
-//	str = ft_ftoa(va_arg(arg, double));
-//
-//	if (!percents->precision)
-//		percents->precision = 6;
-//	if (percents->precision)
-//		if (str[percents->precision] > 5 + '0')
-//		{
-//			str[i] = 0 + '0';
-//			str[i - 1] += 1 + '0';
-//		}
-//	ft_strncat(str, "\0", percents->precision + 1);
-//
-//	ft_putstr(str);
-//	return (TRUE);
-//}
-//
-//
