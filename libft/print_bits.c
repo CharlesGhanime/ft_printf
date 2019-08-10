4e98a3ad4aa0ceb258ac2825241a3dce75972e68
Alban BOITIER <aboitier@e1r12p7.42.fr>
2019-08-11 00:27:13 +0200

    rm main from print_bits, rm addonecharpostest

--- libft/print_bits.c
+++ libft/print_bits.c
@@ -6,7 +6,7 @@
 /*   By: cghanime <cghanime@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/05 17:11:24 by cghanime          #+#    #+#             */
-/*   Updated: 2019/04/29 15:21:48 by cghanime         ###   ########.fr       */
+/*   Updated: 2019/08/11 00:25:04 by aboitier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,4 @@ void ft_print_bits(unsigned int octet)
 		(octet & i ? write(1, "1", 1) : write(1, "0", 1));
 		octet >>= i;
 	}
-}
-
-int main()
-{
-	float octet = 3.14;
-	ft_print_bits(octet);
-	return(0);
 }
