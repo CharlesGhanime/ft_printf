ee90c6fa160d1021f1f9edecc315f6b16bf83c78
Charles <ghanime.charles@gmail.com>
2019-08-28 21:25:15 +0200

    compilation avec flags

--- lib/ft_uintmaxtoa_base.c
+++ lib/ft_uintmaxtoa_base.c
@@ -6,7 +6,7 @@
 /*   By: cghanime <cghanime@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/28 23:58:57 by cghanime          #+#    #+#             */
-/*   Updated: 2019/08/28 05:56:45 by cghanime         ###   ########.fr       */
+/*   Updated: 2019/08/28 21:24:48 by cghanime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,13 @@ char	*ft_uintmaxtoa_base(uint64_t value, char *base)
 	uint64_t			base_length;
 
 	base_length = ft_strlen(base);
-	count = (value < 0) ? 2 : 1;
-	tmp = (value < 0) ? -value : value;
+	count = 1;
+	tmp = value;
 	while (tmp >= base_length && (tmp /= base_length))
 		++count;
-	tmp = (value < 0) ? -value : value;
+	tmp = value;
 	if (!(res = (char*)malloc(sizeof(char) * (count + 1))))
 		return (NULL);
-	if (value < 0)
-		res[0] = '-';
 	res[count] = '\0';
 	while (tmp >= base_length)
 	{
